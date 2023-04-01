02e4657c14c9e60a574fe84fadd92b09b02d1eeb
Justin Ochoa <just.och92@gmail.com>
2023-04-01 13:36:17 -0400

    ui complete

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { Container, Box } from "@chakra-ui/react";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import TextInput from "./components/TextInput";
+
+function App() {
+  const extractKeywords = (text) => {
+    console.log(text);
+  };
+  return (
+    <Box bg="blue.600" color="white" height="100vh" paddingTop={130}>
+      <Container maxW="3xl" centerContent>
+        <Header />
+        <TextInput extractKeywords={extractKeywords} />
+        <Footer />
+      </Container>
+    </Box>
+  );
+}
+
+export default App;
